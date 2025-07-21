4e8e73cc21795c1585350b3ebb95b604a134c240
ryosan-470 <rskjtwp@gmail.com>
2025-07-21 16:10:27 +0900

    test: add wait logic

--- storage/dynamodb/lock_test.go
+++ storage/dynamodb/lock_test.go
@@ -2,6 +2,7 @@ package dynamodb
 
 import (
 	"context"
+	"net/http"
 	"testing"
 	"time"
 
@@ -18,7 +19,11 @@ func setupDynamoDBLocal(t *testing.T) (*dynamodb.Client, func()) {
 	ctx := context.Background()
 
 	// Create wait strategy focusing on port availability and service readiness
-	waitStrategy := wait.ForExposedPort().
+	waitStrategy := wait.ForHTTP("/").
+		WithPort("8000/tcp").
+		WithStatusCodeMatcher(func(status int) bool {
+			return status == http.StatusBadRequest
+		}).
 		WithStartupTimeout(2 * time.Minute).
 		WithPollInterval(1 * time.Second)
 
