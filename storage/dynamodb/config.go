2b8bc1c2823cb473f27ef48e02727409f70bc2b3
ryosan-470 <rskjtwp@gmail.com>
2025-07-27 21:32:23 +0900

    refactor: token bucket

--- storage/dynamodb/config.go
+++ storage/dynamodb/config.go
@@ -9,29 +9,19 @@ import (
 )
 
 type BucketBackendConfig struct {
-	client        *dynamodb.Client
-	tableName     string
-	dimension     string
-	lockTableName string
-	lockTTL       time.Duration
-	lockMaxTries  uint
-	lockMaxTime   time.Duration
+	client    *dynamodb.Client
+	tableName string
 }
 
-func NewBucketBackendConfig(client *dynamodb.Client, tableName, dimension, lockTableName string, lockTTL time.Duration, lockMaxTries uint, lockMaxTime time.Duration) *BucketBackendConfig {
+func NewBucketBackendConfig(client *dynamodb.Client, tableName string) *BucketBackendConfig {
 	return &BucketBackendConfig{
-		client:        client,
-		tableName:     tableName,
-		dimension:     dimension,
-		lockTableName: lockTableName,
-		lockTTL:       lockTTL,
-		lockMaxTries:  lockMaxTries,
-		lockMaxTime:   lockMaxTime,
+		client:    client,
+		tableName: tableName,
 	}
 }
 
 // NewLimitersBackend creates a new TokenBucketDynamoDB instance implemented by the limiters package.
-func (b *BucketBackendConfig) NewLimitersBackend(ctx context.Context, enableRaceHandling bool) (BucketBackendInterface, error) {
+func (b *BucketBackendConfig) NewLimitersBackend(ctx context.Context, dimension string, enableRaceHandling bool) (BucketBackendInterface, error) {
 	props, err := limiters.LoadDynamoDBTableProperties(ctx, b.client, b.tableName)
 	if err != nil {
 		return nil, err
@@ -39,7 +29,7 @@ func (b *BucketBackendConfig) NewLimitersBackend(ctx context.Context, enableRace
 
 	return limiters.NewTokenBucketDynamoDB(
 		b.client,
-		b.dimension,
+		dimension,
 		props,
 		time.Duration(1*time.Second), // Default fill rate
 		enableRaceHandling,
@@ -47,7 +37,7 @@ func (b *BucketBackendConfig) NewLimitersBackend(ctx context.Context, enableRace
 }
 
 // NewCustomBackend creates a new Bucket instance implemented by this package.
-func (b *BucketBackendConfig) NewCustomBackend(ctx context.Context) (BucketBackendInterface, error) {
+func (b *BucketBackendConfig) NewCustomBackend(ctx context.Context, dimension string) (BucketBackendInterface, error) {
 	props, err := limiters.LoadDynamoDBTableProperties(ctx, b.client, b.tableName)
 	if err != nil {
 		return nil, err
@@ -55,19 +45,37 @@ func (b *BucketBackendConfig) NewCustomBackend(ctx context.Context) (BucketBacke
 
 	return NewBackend(
 		b.client,
-		b.dimension,
+		dimension,
 		props,
 		time.Duration(1*time.Second), // Default fill rate
 	), nil
 }
 
-func (b *BucketBackendConfig) NewLock(lockID string) *Lock {
+type LockBackendConfig struct {
+	client        *dynamodb.Client
+	lockTableName string
+	lockTTL       time.Duration
+	lockMaxTries  uint
+	lockMaxTime   time.Duration
+}
+
+func NewLockBackendConfig(client *dynamodb.Client, tableName string, ttl time.Duration, maxTries uint, maxTime time.Duration) *LockBackendConfig {
+	return &LockBackendConfig{
+		client:        client,
+		lockTableName: tableName,
+		lockTTL:       ttl,
+		lockMaxTries:  maxTries,
+		lockMaxTime:   maxTime,
+	}
+}
+
+func (l *LockBackendConfig) NewLockBackend(lockID string) limiters.DistLocker {
 	return NewLock(
-		b.client,
-		b.lockTableName,
+		l.client,
+		l.lockTableName,
 		lockID,
-		b.lockTTL,
-		WithBackoffMaxTries(b.lockMaxTries),
-		WithBackoffMaxTime(b.lockMaxTime),
+		l.lockTTL,
+		WithBackoffMaxTime(l.lockMaxTime),
+		WithBackoffMaxTries(l.lockMaxTries),
 	)
 }
